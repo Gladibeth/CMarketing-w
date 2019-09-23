eaf0d1c8131ec80c7dc367c1485c64af60cb441c
patricia <yively.pa@gmail.com>
2019-09-22 23:15:21 -0400

    add slider de clientes

--- docs/assets/js/setting-slick.js
+++ docs/assets/js/setting-slick.js
@@ -1,39 +1,39 @@
-// $('.main-banner__content').slick({
-//   infinite: true,
-//   autoplay: false,
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-//   dots: true,
-//   arrows: false,
-//   responsive: [{
-//       breakpoint: 1200,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         infinite: true,
-//         dots: false
-//       }
-//     },
-//     {
-//       breakpoint: 900,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1
-//       }
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         dots: false,
-//         autoplay: true,
-//         arrows: false,
-//         autoplaySpeed: 1000
-//       }
-//     }
-//   ]
-// });
+$('.main_clientes').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  dots: true,
+  arrows: false,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
 
 $('.main-about__block').slick({
   infinite: true,
